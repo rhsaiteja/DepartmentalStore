45ca9a1b06e5b297ea64c70439a30601bbaef4fe
rhsaiteja <36411458+rhsaiteja@users.noreply.github.com>
2019-05-01 17:06:27 +0530

    Add files via upload
    Old project upload

---
+++ src/demoproject/DemoProject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoproject;
+
+import demoproject.allinone;
+
+/**
+ *
+ * @author K N G ADITHYA
+ */
+public class DemoProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        allinone k = new allinone();
+        k.setVisible(true);
+        k.setSize(400,350);
+    }
+    
+}
